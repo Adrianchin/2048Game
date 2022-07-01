02a75a1722691184f621d1bbc75dc5fdf4103db1
Adrianchin <98614997+Adrianchin@users.noreply.github.com>
2022-06-30 20:45:55 -0600

    Forgot to uncomment out listen

--- gameProject.js
+++ gameProject.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-/*
+
 app.listen(PORT = 3000, ()=> {
     console.log(`app is running on ${PORT}`)
 })
-*/
+
 
 app.get("/newGame", (req, res) => {//Creates new game (endpoint)
     create2048()
